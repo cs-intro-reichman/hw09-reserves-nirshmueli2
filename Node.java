e320208d8ce716b1683d3f64a8d2794a561e4ae0
Nir Shmueli <nirshmueli2@gmail.com>
2024-05-01 12:11:54 +0300

    try 1

--- Node.java
+++ Node.java
@@ -3,26 +3,26 @@
 public class Node {
    
     // a pointer to a CharData object
-    CharData cp;
+    CharData cd;
  
     // a pointer to a Node object
     Node next;  // pointer
  
     /** Constructs a node with the given CharData object.
      *  The new node will point to the given next node. */
-    public Node(CharData cp, Node next) {
-       this.cp = cp;
+    public Node(CharData cd, Node next) {
+       this.cd = cd;
        this.next = next;
     }
          
     /** Constructs a node with the given CharData object.
      *  The new node will point to null. */
-    public Node(CharData cp) {
-       this(cp, null);
+    public Node(CharData cd) {
+       this(cd, null);
     }
  
     /** Textual representation of this node. */
     public String toString() {
-       return "" + cp;
+       return "" + cd;
     }
  }
